a13f79b05d38c4205799137b009ca5d90838da49
M. Smith <rubys_place@hotmail.com>
2021-01-09 00:43:25 -0800

    Init commit

---
+++ SwiftJoke/SwiftJokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftJokeApp.swift
+//  SwiftJoke
+//
+//  Created by Matt M Smith on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftJokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
