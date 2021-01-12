5e54cd73cea02b8fe8e1bd5a626db647e20234f7
matt smith <rubys_place@hotmail.com>
2021-01-11 21:52:34 -0800

    Added a placeholder for the Image View

--- SwiftJoke/ContentView.swift
+++ SwiftJoke/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            GifImage()
             Label(text: "Joke")
             Label(text: "Punchline")
         }
@@ -19,5 +20,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 mini")
     }
 }
