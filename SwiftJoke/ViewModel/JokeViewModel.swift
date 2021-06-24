f6b01e5f65171537c92d34c7941bc56a9209d4f2
matt smith <rubys_place@hotmail.com>
2021-06-23 22:42:49 -0700

    Got rid of Joke and made small changes

--- SwiftJoke/ViewModel/JokeViewModel.swift
+++ SwiftJoke/ViewModel/JokeViewModel.swift
@@ -11,27 +11,28 @@ import Foundation
 class JokeViewModel: ObservableObject {
     @Published var setup = "Select a joke type below!"
     @Published var punchline = ""
-        
+    
     var cancellable: AnyCancellable?
     private let service = JokeService()
+    private let punchlineDelay = 2.0
     
     func fetchJoke(of jokeType: JokeType) {
         setup = ""
         punchline = ""
         
-        cancellable = self.service.getJokeReponse(of: jokeType)
-            .sink(receiveCompletion: { (completion) in
-            switch completion {
-            case .finished: break
-            case .failure(let error):
-                print(error)
-            }
-        }, receiveValue: { (response) in
-            guard let jokeBody = response.body.first else { return }
-            self.setup = jokeBody.setup
-            Timer.scheduledTimer(withTimeInterval: 4, repeats: false) { timer in
-                self.punchline = jokeBody.punchline
-            }
-        })
+        cancellable = service.getJokeReponse(of: jokeType)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished: break
+                case .failure(let error):
+                    print(error)
+                }
+            }, receiveValue: { response in
+                guard let jokeBody = response.body.first else { return }
+                self.setup = jokeBody.setup
+                Timer.scheduledTimer(withTimeInterval: self.punchlineDelay, repeats: false) { timer in
+                    self.punchline = jokeBody.punchline
+                }
+            })
     }
 }
