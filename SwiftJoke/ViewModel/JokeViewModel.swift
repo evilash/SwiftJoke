ec954438662e408a81134e973b23ca26af3f3a44
matt smith <rubys_place@hotmail.com>
2021-06-26 15:42:57 -0700

    Refactor
    Move magic numbers into properties
    Added indicator for user to know punchline is coming
    Broke out timer functionality into its own func
    
    Removed tests for now. New branch will be created for that work

--- SwiftJoke/ViewModel/JokeViewModel.swift
+++ SwiftJoke/ViewModel/JokeViewModel.swift
@@ -12,9 +12,17 @@ class JokeViewModel: ObservableObject {
     @Published var setup = "Select a joke type below!"
     @Published var punchline = ""
     
+    private struct Constant {
+        static let dot = "."
+    }
+    
+    private class Loading {
+        static var indicator = ""
+        static var counter = 0.0
+    }
+        
     var cancellable: AnyCancellable?
     private let service = JokeService()
-    private let punchlineDelay = 2.0
     
     func fetchJoke(of jokeType: JokeType) {
         setup = ""
@@ -30,9 +38,25 @@ class JokeViewModel: ObservableObject {
             }, receiveValue: { response in
                 guard let jokeBody = response.body.first else { return }
                 self.setup = jokeBody.setup
-                Timer.scheduledTimer(withTimeInterval: self.punchlineDelay, repeats: false) { timer in
-                    self.punchline = jokeBody.punchline
-                }
+                self.getDelayed(punchline: jokeBody.punchline)
             })
+    }
+    
+    //MARK: - Helper Function
+    private func getDelayed(punchline text: String, punchlineDelay: Double = 4.0) {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            self.display(text, timer: timer, delay: punchlineDelay)
+        }
+    }
+    
+    private func display(_ punchline: String, timer: Timer, delay: Double) {
+        if Loading.counter <= delay {
+            self.punchline = Loading.indicator
+            Loading.indicator.append(Constant.dot)
+            Loading.counter = Loading.counter + 1
+        } else {
+            self.punchline = punchline
+            timer.invalidate()
+        }
     }
 }
